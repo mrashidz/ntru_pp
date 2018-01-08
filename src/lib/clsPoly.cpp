a2862b1259301f78c79538cc8f242821804472eb
mrashidz <rashid.z@gmail.com>
2018-01-08 16:42:44 +0100

    polynomial add and multiply

--- src/lib/clsPoly.cpp
+++ src/lib/clsPoly.cpp
@@ -1,6 +1,67 @@
+#include <iostream>
 #include "clsPoly.h"
-
+namespace Polynomial {
 clsPoly::clsPoly()
 {
 
 }
+
+clsPoly addPoly_mod(clsPoly _p1, clsPoly _p2, unsigned int _m)
+{
+    clsPoly res;
+    for(unsigned int i = 0; i<=_p1.Degree; i++)
+        res.Coef[i]=(_p1.Coef[i]+_p2.Coef[i])%_m;
+    return res;
+}
+
+clsPoly addPoly_mod(int _a, clsPoly _p, unsigned int _m)
+{
+    clsPoly res;
+    for(unsigned int i = 0; i<=_p.Degree; i++)
+        res.Coef[i]=(_p.Coef[i]+_a)%_m;
+    return res;
+}
+
+clsPoly mulPoly_mod(clsPoly _p1, clsPoly _p2, unsigned int _m, unsigned int _degree)
+{
+    clsPoly res;
+//    res.Degree = _degree;
+    for (unsigned int i = 0; i >=_p1.Degree; i++) {
+        for (unsigned int j = 0; j >=_p2.Degree; j++) {
+            unsigned int curr_deg = (i+j) % _degree;
+            res.Coef[curr_deg] = ((_p1.Coef[i] * _p2.Coef[j]) + res.Coef[curr_deg]) % _m;
+        }
+    }
+    return res;
+}
+
+clsPoly mulPoly_mod(int _a, clsPoly _p, unsigned int _m)
+{
+    clsPoly res;
+//    res.Degree = /*_degree*/;
+    for (unsigned int i = 0; i >=_p.Degree; i++)
+        res.Coef[i] = (_p.Coef[i] * _a) % _m;
+    return res;
+}
+
+void print_poly(clsPoly _p)
+{
+    for (unsigned int i =0; i < _p.Degree; i++)
+        std::cout << _p.Coef[i] << " X^" << i << " + ";
+    std::cout << _p.Coef[_p.Degree] << " X^" << _p.Degree << std::endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} // namespace Polynomial
--- src/lib/clsPoly.h
+++ src/lib/clsPoly.h
@@ -1,11 +1,28 @@
 #ifndef CLSPOLY_H
 #define CLSPOLY_H
-
-
-class clsPoly
-{
+#include <vector>
+namespace Polynomial {
+class clsPoly { // TRUNCATED POLYNOMIAL OBJECTS
 public:
     clsPoly();
+    unsigned int Degree; // N-1
+    std::vector<unsigned int> Coef; // a_[0 ~ N-1]
 };
+clsPoly addPoly_mod(clsPoly _p1, clsPoly _p2, unsigned int _m);
+clsPoly mulPoly_mod(clsPoly _p1, clsPoly _p2, unsigned int _m, unsigned int _degree);
+clsPoly addPoly_mod(int _a, clsPoly _p, unsigned int _m);
+clsPoly mulPoly_mod(int _a, clsPoly _p, unsigned int _m);
 
+void print_poly(clsPoly _p);
+
+
+
+
+
+
+
+
+
+
+} // namespace Polynomial
 #endif // CLSPOLY_H
